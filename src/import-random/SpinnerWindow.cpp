4acb8b8f9e44deea7fbec186b34e36f2580d1988
ecyht2 <ecyht2@nottingham.edu.my>
2023-01-17 15:36:57 +0800

    Added comments and docmentation

--- src/import-random/SpinnerWindow.cpp
+++ src/import-random/SpinnerWindow.cpp
@@ -2,6 +2,10 @@
 #include <QGridLayout>
 #include <QGraphicsOpacityEffect>
 
+/** Constructor for SpinnerWindow.
+ *
+ * @param parent The parent of the window.
+ * */
 SpinnerWindow::SpinnerWindow(QWidget *parent) {
     setParent(parent);
 
@@ -43,33 +47,61 @@ SpinnerWindow::SpinnerWindow(QWidget *parent) {
     connect(wheel, &SpinningWheel::stopped, this, &SpinnerWindow::displayWinner);
 }
 
+/** Slot called to update the items on the wheel.
+ *
+ * This is called when the QPlainTextEdit::textChanged() signal is emited from
+ * the QPlainTextEditor (editor). It adds and removes the items on the wheel
+ * accordingly.
+ * */
 void SpinnerWindow::updateWheel() {
+    // Getting all the items
     QStringList items = editor->toPlainText().split('\n', Qt::SkipEmptyParts);
+
+    // Disabling and enabling the spinning the wheel if there is no text
     if (items.size() > 0 && !spun) {
         spin->setDisabled(false);
     } else {
         spin->setDisabled(true);
     }
+
+    // Adding items to wheel
     wheel->updateItems(items);
 }
 
+/** Slot called when the user stared spinning the wheel. */
 void SpinnerWindow::spinWheel() {
     wheel->spin();
     spun = true;
+
+    // Disabling spin button and editor
     spin->setDisabled(true);
     editor->setDisabled(true);
 }
 
+/** Slot called when the spinning wheel stoped spinning.
+ *
+ * This is called when the SpinningWheel::stopped() signal is emited.
+ * It makes the background visible and displays the winner on the screen.
+ * */
 void SpinnerWindow::displayWinner(QString selected) {
     winnerBackground->setStyleSheet("background: white");
     winner->setText("Winner:\n" + selected);
     editor->setDisabled(false);
 }
 
-void SpinnerWindow::resizeEvent(QResizeEvent *) {
+/** Method that handles the logic when the window is resized.
+ *
+ * It changes the size of the winner background and text. The size is based
+ * on the size of the SpinningWheel.
+ *
+ * @param event The resize event.
+ * */
+void SpinnerWindow::resizeEvent(QResizeEvent *event) {
+    // Resizing winner background
     QSize wheelSize = wheel->size();
     winnerBackground->resize(wheelSize);
 
+    // Resizing winner text
     int size = wheelSize.width();
     if (wheelSize.width() > wheelSize.height()) {
         size = wheelSize.height();
--- src/import-random/SpinnerWindow.h
+++ src/import-random/SpinnerWindow.h
@@ -13,20 +13,45 @@
 class SpinnerWindow: public QMainWindow {
     Q_OBJECT
     public:
+        /** Constructor for SpinnerWindow.
+        *
+        * @param parent The parent of the window.
+        * */
         SpinnerWindow(QWidget * = nullptr);
     public slots:
+        /** Slot called to update the items on the wheel.
+        *
+        * This is called when the QPlainTextEdit::textChanged() signal is emited from
+        * the QPlainTextEditor (editor). It adds and removes the items on the wheel
+        * accordingly.
+        * */
         void updateWheel();
+        /** Slot called when the user stared spinning the wheel. */
         void spinWheel();
+        /** Slot called when the SpinningWheel stoped spinning.
+        *
+        * This is called when the SpinningWheel::stopped() signal is emited.
+        * It makes the background visible and displays the winner on the screen.
+        * */
         void displayWinner(QString);
-    private:
-        SpinningWheel *wheel;
-        QPlainTextEdit *editor;
-        QPushButton *spin;
-        QLabel *winner;
-        QWidget *winnerBackground;
-        bool spun = false;
     protected:
+        /** Method that handles the logic when the window is resized.
+        *
+        * It changes the size of the winner background and text. The size is based
+        * on the size of the SpinningWheel.
+        *
+        * @param event The resize event.
+        * */
         void resizeEvent(QResizeEvent *);
+    private:
+        /** The SpinningWheel object. */
+        SpinningWheel *wheel;
+        QPlainTextEdit *editor; /*<The text editor to insert item. */
+        QPushButton *spin; /*<Button to start spining the wheel. */
+        QLabel *winner; /*<Label that displays the winner.  */
+        QWidget *winnerBackground; /*<Background to the winner label. */
+        /** Variable that keep tracks if the SpinningWheel has be spun or not. */
+        bool spun = false;
 };
 
 #endif // WINDOW_H_
